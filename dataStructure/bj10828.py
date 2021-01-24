1aefd91c178e8a86aa776337fbe8b82c6993494a
cjy17 <cjy1705@naver.com>
2021-01-24 16:40:34 +0900

    stack

---
+++ dataStructure/bj10828.py
@@ -0,0 +1,44 @@
+from sys import stdin
+
+
+def push(x):
+    stack.append(x)
+
+
+def pop():
+    if not stack:
+        return -1
+    else:
+        return stack.pop()
+
+
+def size():
+    return len(stack)
+
+
+def empty():
+    return 0 if stack else 1
+
+
+def top():
+    return stack[-1] if stack else -1
+
+
+N = int(stdin.readline().rstrip())
+stack = []
+
+for _ in range(N):
+    input_split = stdin.readline().rstrip().split()
+
+    order = input_split[0]
+
+    if order == "push":
+        push(input_split[1])
+    elif order == "pop":
+        print(pop())
+    elif order == "size":
+        print(size())
+    elif order == "empty":
+        print(empty())
+    elif order == "top":
+        print(top())
